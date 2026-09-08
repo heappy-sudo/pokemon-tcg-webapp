540a7b7f382ca42f05c034959b52fba6dd896667
heappy-sudo <heappy@gmail.com>
2026-09-08 14:09:12 +0200

    chore: initial project scaffold

---
+++ hooks/useCardScanner.ts
@@ -0,0 +1,8 @@
+// TODO: Implement WebRTC + Tesseract.js OCR functionality.
+import { useEffect } from 'react';
+
+export const useCardScanner = () => {
+  useEffect(() => {
+    // Implement camera functionality
+  }, []);
+};
