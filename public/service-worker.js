2b6c8e2ea331e00458da012f673aad47134abbd4
heappy-sudo <heappy@gmail.com>
2026-09-08 14:09:15 +0200

    chore: initial project scaffold

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+  console.log('Service Worker Installing');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
