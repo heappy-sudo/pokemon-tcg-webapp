a9f10cd76bf5d5a67b2cd56e658fa6fe6ff174eb
heappy-sudo <heappy@gmail.com>
2026-09-08 14:09:08 +0200

    chore: initial project scaffold

---
+++ components/ConditionSelector.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ConditionSelector = () => {
+  return (
+    <div className="flex space-x-2">
+      <button>NM</button>
+      <button>LP</button>
+      <button>PL</button>
+      <button>PO</button>
+    </div>
+  );
+};
+
+export default ConditionSelector;
