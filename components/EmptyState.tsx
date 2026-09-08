cdb37eebf1f8d3abfee3ba485c3497962e4e5ba1
heappy-sudo <heappy@gmail.com>
2026-09-08 14:09:16 +0200

    chore: initial project scaffold

---
+++ components/EmptyState.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmptyState = () => {
+  return (
+    <div className="text-center p-4">
+      <h2>No Cards Found</h2>
+      <p>Try searching for cards or adding new ones!</p>
+    </div>
+  );
+};
+
+export default EmptyState;
